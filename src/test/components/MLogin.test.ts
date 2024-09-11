1448f80979aa62940eb95eaa037ce0f8bc660484
n101661 <n101661@gmail.com>
2024-09-12 07:56:15 +0800

    test: fix not correct testing login success

--- src/test/components/MLogin.test.ts
+++ src/test/components/MLogin.test.ts
@@ -3,8 +3,27 @@ import { AxiosHeaders } from "axios"
 import { mount } from "@vue/test-utils"
 import { setActivePinia, createPinia } from "pinia"
 import ElementPlus, { ElMessage } from "element-plus"
-import { login } from "../../client"
 import MLogin from "../../components/MLogin.vue"
+import { useAuthStore } from "../../stores/auth"
+
+vi.mock(import("../../client/services.gen"), async (importOriginal) => {
+  const mod = await importOriginal()
+  return {
+    ...mod,
+    login: vi.fn(mod.login).mockResolvedValue({
+      data: {
+        accessToken: "my-access-token",
+      },
+      status: 200,
+      statusText: "",
+      headers: {},
+      config: {
+        headers: new AxiosHeaders(),
+      },
+      error: undefined,
+    }),
+  }
+})
 
 test("login successful", async () => {
   const pinia = setActivePinia(createPinia())
@@ -17,24 +36,10 @@ test("login successful", async () => {
       },
     },
   })
-  const stubLogin = vi.fn(login)
-  stubLogin.mockResolvedValue({
-    data: {
-      accessToken: "my-access-token",
-    },
-    status: 200,
-    statusText: "",
-    headers: {},
-    config: {
-      headers: new AxiosHeaders(),
-    },
-    error: undefined,
-  })
 
   const elInputs = wrapper.findAllComponents({ name: "ElInput" })
   const userId = elInputs[0]
   const password = elInputs[1]
-  expect(elInputs).toHaveLength(2)
 
   const submitButton = wrapper.findComponent({ name: "ElButton" })
 
@@ -43,4 +48,21 @@ test("login successful", async () => {
   await submitButton.trigger("click")
 
   await wrapper.vm.$nextTick()
+  await promiseDone()
+
+  const successEvent = wrapper.emitted("success")
+  expect(successEvent).toBeTruthy()
+  expect(successEvent?.length).toBe(1)
+  expect(successEvent ? successEvent[0] : []).toEqual([
+    "my-id",
+    "my-access-token",
+  ])
+
+  const authStore = useAuthStore()
+  expect(authStore.userId).toEqual("my-id")
+  expect(authStore.accessToken).toEqual("my-access-token")
 })
+
+function promiseDone(): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve))
+}
